0e32f4da732ede1681411b815d73a86e84a9b9d0
R0x0s <102767043+R0x0s@users.noreply.github.com>
2023-06-21 11:33:42 -0700

    finito

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,18 +1,50 @@
 import { useState, useEffect } from 'react';
 
 import {copy, linkIcon, loader, tick } from '../assets'
-import { Input } from 'postcss';
+import { useLazyGetSummaryQuery } from '../services/article';
 
 
 const Demo = () => {
 
-  const [artile, setArticle] = useState({
+  const [article, setArticle] = useState({
     url:'',
     summary: '',
   })
 
+  const [allArticles, setAllArticles] = useState([]);
+
+  const [getSummary, {error, isFetching }] =
+  useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage =JSON.parse(
+    localStorage.getItem('articles')
+    )
+
+    if(articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage)
+    }
+  }, []);
+
   const handleSubmit = async (e) => {
-    alert('Submitted')
+    e.preventDefault();
+
+    const {data} = await getSummary({ articleUrl: article.url}); 
+    
+    if(data?.summary) {
+      const newArticle = { ...article, summary:data.summary};
+      
+      const updateAllArticles = [newArticle, ...
+      allArticles];
+
+      setArticle(newArticle);
+      setAllArticles(updateAllArticles);
+
+      localStorage.setItem('articles', JSON.stringify
+      (updateAllArticles));
+
+      console.log(newArticle);
+    }
   }
 
   return (
@@ -22,7 +54,7 @@ const Demo = () => {
         <form 
         className="relative flex
         justify-center items-center"
-        onsubmit={handleSubmit}
+        onSubmit={handleSubmit}
         >
           <img 
           src={linkIcon}
@@ -35,7 +67,7 @@ const Demo = () => {
           placeholder="Enter URL"
           value={setArticle.url}
           onChange={(e) => setArticle ({ ... 
-          artile, url: e.target.value})} 
+          article, url: e.target.value})} 
           required
           className="url_input peer"
           />
@@ -50,11 +82,88 @@ const Demo = () => {
           </button>
         </form>
         {/* Browse URL History */}
+        <div className="flex flex-col gap-1 max-h-60
+        overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+            key={`link-${index}`}
+            onClick={() => setArticle(item)}
+            className="link_card"
+            >
+              <div className="copy_btn">
+                <img
+                src={copy}
+                alt="copy_icon"
+                className="w-[40%] h-[40%] object-contain"
+                />
+                </div>
+                <p className="flex-1 font-satoshi
+                text-blue-700 font-meduim text-sm truncate">
+                  {item.url}
+                </p>
+              </div>
+          ))}
+          </div>
       </div>
 
       {/* Display Results */}
+
+      {/* <div className="my-10 max-w-full flex justify-center
+      items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20
+          h-20 object-contain"/>
+        ) : error ? (
+          <p className="font-inter font-bold text-black 
+          text-center">
+            Well, that wasnt supposed to happen...
+            <br />
+            <span className="font-satoshi font-normal 
+            text-gray-700">
+              {error?.data?.error}
+            </span>
+
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+          )
+        )
+        )}
+      </div>
+
      </section>
   )
-}
+}*/}
+
+<div className='my-10 max-w-full flex justify-center items-center'>
+        {isFetching ? (
+          <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+        ) : error ? (
+          <p className='font-inter font-bold text-black text-center'>
+            Well, that wasn't supposed to happen...
+            <br />
+            <span className='font-satoshi font-normal text-gray-700'>
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className='flex flex-col gap-3'>
+              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                Article <span className='blue_gradient'>Summary</span>
+              </h2>
+              <div className='summary_box'>
+                <p className='font-inter font-medium text-sm text-gray-700'>
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
+    </section>
+  );
+};
 
 export default Demo

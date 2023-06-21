ddee5bcc423c411b9cba54c9f46167735cc278ee
unknown <troysteedmiller@gmail.com>
2023-06-21 13:53:28 -0700

    added dist

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/Article_summarizer',
+    plugins: [react()],
 })
